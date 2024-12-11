0beeb31668c59bff1469599b4c1faaf50329c883
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-12-11 21:08:04 +0100

    Add files via upload

--- TIC_TAC_TOE/tic_tac_toe_oop.py
+++ TIC_TAC_TOE/tic_tac_toe_oop.py
@@ -1,4 +1,5 @@
-from board_oop import Board
+from board_oop import Board, check_user_input
+import re
 
 playing_board =Board()
 
@@ -6,18 +7,20 @@ playing_board.print_board()
 
 counter = 0
 is_on = True
+
 while counter < 9 and is_on:
     if counter % 2 == 0:
         user_num = 1
     else:
         user_num = 2
-    user_choice = input(f"Enter your choice player {user_num}: ")
-    playing_board.modify_board(user_choice= user_choice, 
-                               user_num=user_num)
+    is_correct, user_choice = check_user_input(user_num=user_num)
+    if is_correct:
+        playing_board.modify_board(user_choice= user_choice, 
+                                   user_num=user_num)
 
-    is_on = playing_board.check_score(user_num=user_num)
-    
-    if counter == 8:
-        is_on = False
-        playing_board.check_for_draw()
-    counter += 1
+        is_on = playing_board.check_score(user_num=user_num)
+        
+        if counter == 8:
+            is_on = False
+            playing_board.check_for_draw()
+        counter += 1
